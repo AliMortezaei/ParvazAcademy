00319c74f83caa3562158ccbf5a6b679b0d3c2fa
AlirezMortezaei <Mortezaei2324@gmail.com>
2024-01-22 15:32:47 +0330

    🔧 config project backend
    🔧 config env file end settings

---
+++ src/backend/app/core/wsgi.py
@@ -0,0 +1,27 @@
+"""
+WSGI config for app project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+import pathlib
+
+import dotenv
+
+from django.core.wsgi import get_wsgi_application
+
+
+CURRENT_DIR = pathlib.Path(__file__).resolve().parent
+BASE_DIR = CURRENT_DIR.parent
+ENV_FILE_PATH = BASE_DIR / ".env"
+
+dotenv.read_dotenv(str(ENV_FILE_PATH))
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+application = get_wsgi_application()
