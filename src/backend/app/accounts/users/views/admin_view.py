2e2fb06e20feb610b3bf438577407a6f727418b3
AlirezMortezaei <Mortezaei2324@gmail.com>
2024-01-29 09:13:45 +0330

    🚧 Update feature in User
    🚨 user admin in update user type fix

--- src/backend/app/accounts/users/views/admin_view.py
+++ src/backend/app/accounts/users/views/admin_view.py
@@ -2,16 +2,16 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from accounts.users.models import User
+from accounts.users.models import User, UserType
 from accounts.users.serializers.admin_serializer import AdminUserSeialiser, AdminUserCreateSeialiser
 
 
 class AdminUserViewSet(ModelViewSet):
 
-    serializer_class = AdminUserSeialiser
     queryset = User.objects.all()
     
     permission_classes = [IsAdminUser]
+
 
     def  get_serializer_class(self):
         if self.action == 'create':
@@ -19,6 +19,18 @@ class AdminUserViewSet(ModelViewSet):
         else:
             return AdminUserSeialiser
 
+
+    def partial_update(self, request, *args, **kwargs):
+        kwargs['partial'] = True
+        match request.data['user_type']:
+            case 'teacher':
+                user_type = UserType.objects.get(user_type="teacher")
+                request.data['user_type'] = user_type
+            case 'student':
+                user_type = UserType.objects.get(user_type="student")
+                request.data['user_type'] = user_type
+        return self.update(request, *args, **kwargs)
+        
     
     def perform_create(self, serializer):
         self.get_queryset().model.objects.create_user(
