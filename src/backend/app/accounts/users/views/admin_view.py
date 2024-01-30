9eaeade0002fd8683e78deb4722dee993b327083
AlirezMortezaei <Mortezaei2324@gmail.com>
2024-01-30 12:16:03 +0330

    🔨 Add filter get user
    🔨 use django_filter add filter for date_joined and is active user and user types show api

--- src/backend/app/accounts/users/views/admin_view.py
+++ src/backend/app/accounts/users/views/admin_view.py
@@ -1,16 +1,37 @@
 from rest_framework.permissions import IsAdminUser
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from django_filters import rest_framework as filters
 
 from accounts.users.models import User, UserType
 from accounts.users.serializers.admin_serializer import AdminUserSeialiser, AdminUserCreateSeialiser
 
 
-class AdminUserViewSet(ModelViewSet):
 
-    queryset = User.objects.all()
+
+class AdminUserFilter(filters.FilterSet):
+    """
+        filter for fields registery date (date_joined)
+        and user type (student, teacher, admin) 
+        and users active accounts
+    """    
+    date_year = filters.NumberFilter(field_name='date_joined', lookup_expr='year')
+    date_month = filters.NumberFilter(field_name='date_joined', lookup_expr='month')
+    date_day = filters.NumberFilter(field_name='date_joined', lookup_expr='day')
+
+
+    class Meta:
+        model = User
+        fields = ['user_type', 'date_joined','is_active']
+        
+        
+class AdminUserViewSet(ModelViewSet):
     
-    permission_classes = [IsAdminUser]
+    queryset = User.objects.all()
+    filter_backends = (filters.DjangoFilterBackend,)
+    filterset_class = AdminUserFilter
+
+    #permission_classes = [IsAdminUser]
 
 
     def  get_serializer_class(self):
